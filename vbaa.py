b8444705602f91b8703330da689ce40e67dd7d16
serbekun <sergey994049@gmail.com>
2025-03-29 13:42:53 +0900

    Update vbaa.py

--- vbaa.py
+++ vbaa.py
@@ -1,9 +1,25 @@
 import random
 
-def bot_select_action(player_hp, player_attack, player_heal, player_block, bot_hp, bot_attack, bot_heal, bot_block):
+def bot_select_action(player_hp, player_attack, player_heal, player_block, bot_hp, bot_attack, bot_heal, bot_block, round_count):
 
     if bot_hp < 35:
         return 2
     
     if player_hp < 20:
         return 1
+
+    if round_count < 5:
+        return 5
+    
+    bot_action = random.randint(1, 5)
+
+    if bot_action == 1:
+        return 1
+    elif bot_action == 2:
+        return 2
+    elif bot_action == 3:
+        return 3
+    elif bot_action == 4:
+        return 4
+    elif bot_action == 5:
+        return 5
